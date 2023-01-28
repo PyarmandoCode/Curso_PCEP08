cb5552c9876c64823b825c9796b286b658c64abf
hola.pyarmando.academy@gmail.com <hola.pyarmando.academy@gmail.com>
2023-01-28 11:55:42 -0500

    claseo1

---
+++ Variables.py
@@ -0,0 +1,25 @@
+# todo Tipo de datos primitivos
+edad_per = 28
+nombre_per = "Python"
+precio_prod = 20.8
+cantidad_prod = 100
+estado_prod = True
+
+print(type(edad_per))
+print(type(nombre_per))
+print(type(precio_prod))
+print(type(str(cantidad_prod)))
+print(type(estado_prod))
+
+listas_personas = ['JUAN', 200, 20.90]  # lIST
+sueldos = (1990, 27768, 3545) #tuple
+empleados = {'codigo':100,'nombre':"Curso PCEP"} #dic
+
+
+print(type(sueldos))
+print(type(listas_personas))
+print(type(empleados))
+
+basket = ['apple', 'orange', 'apple', 'pear', 'orange', 'banana']
+for f in sorted(set(basket)):
+    print(f)
